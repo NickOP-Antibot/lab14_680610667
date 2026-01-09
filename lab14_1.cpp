29632a6632d3c34840cc78bce53d5935d840aa5f
NickOP-Antibot <chitsanupat2549@gmail.com>
2026-01-09 14:46:41 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int i = 1; i < N; i++){
+		T key = d[i];
+		int j = i - 1;
+		while(j >= 0 && d[j] < key){
+			d[j + 1] = d[j];
+			j = j - 1;
+		}
+		d[j + 1] = key;
+		
+		cout << "Pass " << i << ":";
+		for(int j = 0; j < N; j++) cout << d[j] << " ";
+		cout << endl;
+	}
 }
 
 int main(){
